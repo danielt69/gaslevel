7cfb7ccb7e05fa63ef5a8a9d67235ccd967a3e9d
Daniel <danielt69@gmail.com>
2014-06-20 15:23:13 +0300

    clean overrides folder

--- app/overrides/logo.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/home',
-         :name => 'insert_home',
-         :insert_after => '#content',
-         :text => '<div class="elad">ELADELAD</div><span class="elad2"></span>',
-)
