64d42b4de45df1fbd3cebe7d517a4301235ac486
eladamrani <eladamrani@gmail.com>
2014-06-21 00:21:14 +0300

    lagas 0.1

--- app/overrides/header.rb
+++ app/overrides/header.rb
@@ -10,11 +10,4 @@ Deface::Override.new(:virtual_path => 'spree/shared/_header',
          :insert_top => '#header[data-hook]',
          :text          => '<a class="gaslevel_image" href="http://www.gaslock.com"><img src="/images/gaslevel.png" alt="GASLevel"></a>',
 )
-Deface::Override.new(:virtual_path => 'spree/shared/_header',
-         :name => 'search_bar',
-         :replace_contents => '#search-bar[data-hook]',
-         :text          => '<div class="search_bar"></div>',
-)
-
-
 # :replace_contents
