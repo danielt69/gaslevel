b950f904011ec49abb166a0fab151795155adc53
Daniel <danielt69@gmail.com>
2014-06-20 15:52:08 +0300

    push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ Htdocs::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
+  
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
