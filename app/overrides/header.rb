218001a0356c994057819a9877eb765ba8abb9fb
eladamrani <eladamrani@gmail.com>
2014-06-21 00:50:27 +0300

    lagas 0.3

--- app/overrides/header.rb
+++ app/overrides/header.rb
@@ -10,4 +10,14 @@ Deface::Override.new(:virtual_path => 'spree/shared/_header',
          :insert_top => '#header[data-hook]',
          :text          => '<a class="gaslevel_image" href="http://www.gaslock.com"><img src="/images/gaslevel.png" alt="GASLevel"></a>',
 )
+
+Deface::Override.new(:virtual_path => 'spree/shared/_header',
+         :name => 'my_account',
+         :replace_contents => '#nav-bar .pos_0 a',
+         :text          => '<a href="/account">התחבר</a>',
+)
+
+
+#:insert_after
+#:insert_top
 # :replace_contents
