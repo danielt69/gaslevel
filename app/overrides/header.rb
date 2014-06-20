bd7068ef4b9dc2e67a92526fa175c6b444ec95fa
eladamrani <eladamrani@gmail.com>
2014-06-20 23:27:49 +0300

    we

--- app/overrides/header.rb
+++ app/overrides/header.rb
@@ -5,6 +5,11 @@ Deface::Override.new(:virtual_path => 'spree/shared/_header',
          :text          => '',
 )
 
+Deface::Override.new(:virtual_path => 'spree/shared/_header',
+         :name => 'gaslevel_image',
+         :insert_top => '#header[data-hook]',
+         :text          => '<a href="http://www.gaslock.com"><img src="/images/gaslevel/png" alt="GASLevel"></a>',
+)
 
 
 
