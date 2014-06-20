d443bc696924eb1fe52b7f6a3030c595e27706ff
eladamrani <eladamrani@gmail.com>
2014-06-20 13:47:23 +0300

    LOGO daaneil

---
+++ app/overrides/logo.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => 'spree/home',
+         :name => 'insert_home',
+         :insert_after => '#content',
+         :text => '<div class="elad">ELADELAD</div><span class="elad2"></span>',
+)
