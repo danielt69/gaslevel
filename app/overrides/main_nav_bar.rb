4a9036be491f661b0b76cacd1ca823e411ff2ea3
eladamrani <eladamrani@gmail.com>
2014-06-21 09:53:21 +0300

    remove google link

--- app/overrides/main_nav_bar.rb
+++ app/overrides/main_nav_bar.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path => 'spree/shared/_main_nav_bar',
-         :name => 'insert_main_nav_bar',
-         :insert_top => '#main-nav-bar[data-hook]',
-         :text          => '<li><a href="https://www.google.co.il">google</a></li><br/>',
-)
+# Deface::Override.new(:virtual_path => 'spree/shared/_main_nav_bar',
+#          :name => 'insert_main_nav_bar',
+#          :insert_top => '#main-nav-bar[data-hook]',
+#          :text          => '<li><a href="https://www.google.co.il">google</a></li><br/>',
+# )
